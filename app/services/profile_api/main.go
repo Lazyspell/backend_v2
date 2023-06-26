946a634f39fb0271b1b9ee523a56e0ba391a25a4
lazyspell <jeremy2975@yahoo.com>
2023-06-25 20:48:52 -0500

    adding logging to profile-api

--- app/services/profile_api/main.go
+++ app/services/profile_api/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
 	"runtime"
+	"syscall"
 
 	"github.com/jelam2474/backend_v2/foundation/logger"
 	"go.uber.org/zap"
 )
+
+var build = "develop"
 
 func main() {
 	log, err := logger.New("PROFILE-API")
@@ -30,6 +34,12 @@ func run(log *zap.SugaredLogger) error {
 	// GOMAXPROCS
 
 	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	defer log.Infow("shdudown")
+
+	shudown := make(chan os.Signal, 1)
+	signal.Notify(shudown, syscall.SIGINT, syscall.SIGABRT)
+	<-shudown
+
 	return nil
 
 }
